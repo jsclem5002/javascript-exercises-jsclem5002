f807c4a88291d38d7cea1b2fa32c1ac29da678e8
jsclem5002 <18202458+jsclem5002@users.noreply.github.com>
2025-06-13 17:03:02 -0400

    Completed exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  let temp = 0;
+  let sum = 0;
+  if (min > max) {
+    temp = max;
+    max = min;
+    min = temp;
+  } else if (
+    min < 0 ||
+    max < 0 ||
+    !Number.isInteger(min) ||
+    !Number.isInteger(max)
+  ) {
+    return "ERROR";
+  }
 
+  for (i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
